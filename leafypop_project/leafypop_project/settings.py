878724eba5b4d9201d7483a806f6121a5d75bb62
ayushi1115 <ayushisp1132@gmail.com>
2026-01-20 23:28:23 +0530

    Secure settings: Use env vars for Secret Key and Debug

--- leafypop_project/leafypop_project/settings.py
+++ leafypop_project/leafypop_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2k52479k8=&05tn26^kqtrn*3k94!y2@&my(c==(bbuqc)&+fp'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key-default-change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
