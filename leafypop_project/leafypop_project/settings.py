875b21d1a4c519858beebf594ff5a2262aa0d352
ayushi1115 <ayushisp1132@gmail.com>
2026-01-25 23:56:04 +0530

    Enable PostgreSQL support via DATABASE_URL environment variable

--- leafypop_project/leafypop_project/settings.py
+++ leafypop_project/leafypop_project/settings.py
@@ -76,14 +76,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'leafypop_project.wsgi.application'
 
 
+import dj_database_url
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Use SQLite if DATABASE_URL is not set
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600
+    )
 }
 
 
